15329b3483fe1f6c40026dbe95ed5cc925b806d3
Cornelius Keller <cke@marketlogicsoftware.com>
2016-04-27 09:29:44 +0200

    fix confd / etcd

--- src/boot.sh
+++ src/boot.sh
@@ -19,16 +19,22 @@ if [ ! -z "$ETCD_CLEINT_CA" ]; then
 	_ETCD_CLEINT_CA=" -client-ca-keys $ETCD_CLEINT_CA"
 fi
 
+CONFD_OPTS=" $_ETCD $_ETCD_CLEINT_KEY $_ETCD_CLEINT_CA $_ETCD_CLEINT_CERT -config-file /etc/confd/confd.toml"
+
+
+
 echo "[nginx] booting container. ETCD: $ETCD ETCD_CLEINT_CERT: $ETCD_CLEINT_CERT ETCD_CLEINT_KEY: $ETCD_CLEINT_KEY ETCD_CLEINT_CA: $ETCD_CLEINT_CA"
 
+echo "CONFD_OPTS = $CONFD_OPTS"
+
 # Loop until confd has updated the nginx config
-until confd -onetime $_ETCD $_ETCD_CLEINT_KEY $_ETCD_CLEINT_CA  -config-file /etc/confd/confd.toml; do
+until confd -onetime $CONFD_OPTS; do
   echo "[nginx] waiting for confd to refresh nginx.conf"
   sleep 5
 done
 
 # Run confd in the background to watch the upstream servers
-confd -interval 10 -node $ETCD -config-file /etc/confd/confd.toml &
+confd -interval 10 $CONFD_OPTS -config-file /etc/confd/confd.toml &
 echo "[nginx] confd is listening for changes on etcd..."
 
 # Start nginx
