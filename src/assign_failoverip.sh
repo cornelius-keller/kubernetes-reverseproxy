c60d58d1cc49647fb292f86d1f65dcefb538b93e
Cornelius Keller <cke@marketlogicsoftware.com>
2016-06-06 18:39:27 +0200

    some debug output

--- src/assign_failoverip.sh
+++ src/assign_failoverip.sh
@@ -7,4 +7,6 @@ HOST_IP=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
    https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/default/pods/$HOSTNAME \
    | grep hostIP | awk '{ print $2 }' | tr -d  ',"')
 
+echo "setting Failover IP:  $FAILOVER_IP to Server IP:  $HOST_IP"
+
 curl -u $HETZNER_USER:$HETZNER_PASS https://robot-ws.your-server.de/failover/$FAILOVER_IP -d active_server_ip=$HOST_IP
