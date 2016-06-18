06b9252dc06fcd2fe7bdafdc8eb16ce23e3ffb2f
Cornelius Keller <cke@marketlogicsoftware.com>
2016-06-18 16:07:51 +0200

    overwrite ETCD variable with host ip to make sure not to talk to a dead etcd node

--- src/assign_failoverip.sh
+++ src/assign_failoverip.sh
@@ -6,6 +6,7 @@ KUBE_TOKEN=$(</var/run/secrets/kubernetes.io/serviceaccount/token)
 HOST_IP=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
    https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/default/pods/$HOSTNAME \
    | grep hostIP | awk '{ print $2 }' | tr -d  ',"')
+ETCD=https://$HOST_IP:2379
 
 echo "setting Failover IP:  $FAILOVER_IP to Server IP:  $HOST_IP"
 
